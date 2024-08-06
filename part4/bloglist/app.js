d128d5ef148ca3a01d95011c09f10b61aa3f2478
Wyatt <wyatt.furois@gmail.com>
2024-08-05 21:55:11 -0500

    building out testing

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,13 @@
 const config = require('./utils/config')
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = express()
+const api = supertest(app)
 
 mongoose.set('strictQuery', false)
 
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,15 +1,87 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[2])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[3])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[4])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[5])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('the first blog is about react patterns', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(e => e.title)
+  assert(contents.includes('React patterns'))
+})
+
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "async/await simplifies making async calls",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD809.html",
+    likes: 20
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(n => n.title)
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test('a blog without a title is not added', async () => {
+  const newBlog = {
+    title: "",
+    author: "Empty A. Title",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD809.html",
+    likes: 6
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
 after(async () => {
